f0bb900fa4092a7ba6cbd91a2c8deef1480758a5
heun630 <egg6030@gmail.com>
2024-12-03 14:13:36 +0900

    fix: change update.sh

--- update.sh
+++ update.sh
@@ -2,11 +2,19 @@
 
 cd /home/0xheun
 
+# 최신 코드 가져오기
 git fetch --all
 git reset --hard origin/main
 
+# 모든 파일에 실행 권한 추가
 chmod -R +x /home/0xheun
 
+# Yarn 캐시 정리
+yarn cache clean
+
+# 종속성 설치
 yarn install
+
+# PM2 프로세스 재시작
 pm2 delete "0xheun blog"
 pm2 start ecosystem.config.js
