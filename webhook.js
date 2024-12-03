93fea4f9f70817209ca7335c1dfd3382aea2ccf7
heun630 <egg6030@gmail.com>
2024-12-03 13:43:35 +0900

    fix: test webhook

--- webhook.js
+++ webhook.js
@@ -5,7 +5,21 @@ const app = express();
 app.use(express.json());
 
 app.post('/update', (req, res) => {
-    if (req.headers['x-github-event'] === 'push') {
+    const event = req.headers['x-github-event'];
+
+    if (!event) {
+        console.error('Missing x-github-event header');
+        return res.status(400).send('Bad Request: Missing event header');
+    }
+
+    if (event === 'ping') {
+        console.log('Ping event received');
+        return res.status(200).send('Pong');
+    }
+
+    if (event === 'push') {
+        console.log('Push event received');
+
         exec('/home/0xheun/update.sh', (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error: ${error.message}`);
@@ -15,10 +29,11 @@ app.post('/update', (req, res) => {
                 console.error(`Stderr: ${stderr}`);
             }
             console.log(`Stdout: ${stdout}`);
-            res.status(200).send('Updated');
+            res.status(200).send('Update successful');
         });
     } else {
-        res.status(400).send('Invalid event');
+        console.error(`Unhandled event: ${event}`);
+        res.status(400).send(`Unhandled event: ${event}`);
     }
 });
 
